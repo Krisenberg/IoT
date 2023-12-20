5f41239aaadf2d2e8bce9e9f449ff9071e5ecc57
Krzysztof Glowacz <krzysztofglowacz88@gmail.com>
2023-12-20 08:54:03 +0100

    Resolved some concurrency issues.

--- Lab_10/test_leds.py
+++ Lab_10/test_leds.py
@@ -30,3 +30,27 @@ def ledsDeny(duration):
     pixelsDeny()
     timer = threading.Timer(duration, cleanPixels) #calls the callback after 'duration' time in seconds
     timer.start()
+
+# def ledsAcceptHelper(ledsOnDuration, pauseDuration, count):
+#     for _ in range(count):
+#         pixelsAccept()
+#         time.sleep(ledsOnDuration)  # Pause between beeps
+#         cleanPixels()
+#         time.sleep(pauseDuration)
+
+# def ledsAccept(ledsOnDuration, pauseDuration, count):
+#     thread = threading.Thread(target=ledsAcceptHelper, args=(ledsOnDuration, pauseDuration, count))
+#     thread.start()
+#     thread.join()
+
+# def ledsDenyHelper(ledsOnDuration, pauseDuration, count):
+#     for _ in range(count):
+#         pixelsDeny()
+#         time.sleep(ledsOnDuration)  # Pause between beeps
+#         cleanPixels()
+#         time.sleep(pauseDuration)
+
+# def ledsDeny(ledsOnDuration, pauseDuration, count):
+#     thread = threading.Thread(target=ledsDenyHelper, args=(ledsOnDuration, pauseDuration, count))
+#     thread.start()
+#     thread.join()
